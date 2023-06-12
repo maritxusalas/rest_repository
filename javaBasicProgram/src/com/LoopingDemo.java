785f8bb3b1a3156bd8f21049f68fea1efb5970eb
maritxu.salas <mariasalasvodaf@ip-172-31-23-140.ec2.internal>
2023-06-12 12:02:41 +0000

    finish

---
+++ javaBasicProgram/src/com/LoopingDemo.java
@@ -0,0 +1,26 @@
+package com;
+
+public class LoopingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("While loop");
+//		int i=1, n=10;
+//		while (i<n) {
+//			System.out.println("value of i is "+i);
+//			i++;
+//			
+//		}
+//		System.out.println("Hello");
+		
+		
+	System.out.println("Do While loop");
+    int i=1, n=10;
+    do {
+    	System.out.println("My value is "+i);
+    	i++;
+    	
+    }while(i<=n);
+    System.out.println("finish");
+	}
+}
