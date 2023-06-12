22ee4c4b2e55bb2d3fa5d0d92c8b71893a71e86e
maritxu.salas <mariasalasvodaf@ip-172-31-23-140.ec2.internal>
2023-06-12 10:07:42 +0000

    mis primeros programs chispas

---
+++ javaBasicProgram/src/com/DataTypesDemo.java
@@ -0,0 +1,30 @@
+package com;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		double c = 10.30;
+		boolean result = true;
+		char abc ='$';
+		int sum = a+b;
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println("Value of a is " +a);
+		System.out.println(a+b);
+		System.out.println("The sum of two numbers is " +a+b);
+		System.out.println("The sum of two numbers is " +(a+b));
+		System.out.println("Sum of two number is " +sum);
+		System.out.println("a " +b);
+		System.out.println(c);
+		System.out.println(result);
+		System.out.println(abc);
+		a = a + 1;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+	}
+
+}
---
+++ javaBasicProgram/src/com/Demo.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("viva Java");
+		System.out.println("amazing Java");
+		System.out.println("yabadabadu");
+		System.out.print("Welcome again");
+		System.out.print("Welcome again 2");
+	}
+
+}
