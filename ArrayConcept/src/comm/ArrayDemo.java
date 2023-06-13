05c25243ddc292a1d1eb38c832fe0b147c69d48b
maritxu.salas <mariasalasvodaf@ip-172-31-23-140.ec2.internal>
2023-06-13 08:33:52 +0000

    array concept

---
+++ ArrayConcept/src/comm/ArrayDemo.java
@@ -0,0 +1,17 @@
+package comm;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num[]; // array declaration
+		int num1[]= {10,20,30,40,50};//array declaration with initialization
+		System.out.println(num1);
+		System.out.println(num1[0]);
+		System.out.println(num1[1]);
+		System.out.println(num1[2]);
+		System.out.println(num1[3]);
+		System.out.println(num1[4]);
+	}
+
+}
